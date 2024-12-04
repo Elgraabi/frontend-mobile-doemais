ec4beebf0480a17ef5356f99bbabb0301d3e0a2c
Elgraabi <silvatdb8@gmail.com>
2024-12-04 14:25:15 -0300

    style: criar estilos para o componente Button com tema claro

---
+++ src/components/buttons/button/styles.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../../../theme";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#f5f5f5", 
+    },
+    button: {
+        width: 305,
+        paddingVertical: 8, 
+        backgroundColor: "#b562fd",
+        borderRadius: 34,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    buttonText: {
+        fontFamily: "Inter-SemiBold", 
+        fontWeight: "600",
+        fontSize: 16, 
+        color: theme.light.textButton,
+        textTransform: "none",
+    },
+    default: {
+
+    }
+});
+
+export default styles;
