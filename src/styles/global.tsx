ed37450210af17e041ece54bed72a20467e840ad
Elgraabi <silvatdb8@gmail.com>
2024-12-03 21:16:34 -0300

    feat: initial commit

---
+++ src/styles/global.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../theme";
+
+const global = StyleSheet.create({
+    text: {
+        color: theme.light.text,
+    },
+    title: {
+        color: theme.light.text,
+        backgroundColor: "#1231",
+    },
+    container: {
+      
+    }
+});
+
+export default global
