1f3631f9175ec7d550757d856fdd08808f02bbad
Elgraabi <silvatdb8@gmail.com>
2024-12-04 17:14:52 -0300

    Update: adicionando tela login em appnavigation

---
+++ src/components/inputs/input/styles.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 16,
+        backgroundColor: "#e2dede",
+        borderRadius: 16,
+        marginBottom: 15,
+    },
+    inputText: {
+        paddingLeft: 10,
+        width: "90%",
+        fontWeight: "bold",
+    },
+});
+
+export default styles;
--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LandingPageScreen from "../screens/landingPage";
+import LoginScreen from "../screens/login";
 
 
 
@@ -13,6 +14,8 @@ export default function AppNavigation() {
             <Stack.Navigator initialRouteName="LandingPage" screenOptions={{ headerShown: false }}>
                 
                 <Stack.Screen name="LandingPage" component={LandingPageScreen} />
+
+                <Stack.Screen name="Login" component={LoginScreen} />
                 
             </Stack.Navigator>
         </NavigationContainer>
