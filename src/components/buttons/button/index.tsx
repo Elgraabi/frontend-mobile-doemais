85ee7494db3e8415307b909ee0583ba6b092fa87
Elgraabi <silvatdb8@gmail.com>
2024-12-04 14:24:56 -0300

    feat: criar componente Button com estilos dinâmicos e título

---
+++ src/components/buttons/button/index.tsx
@@ -0,0 +1,27 @@
+import { Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
+import styles from "./styles";
+
+type StyleKeys = 'default';
+
+type ButtonProps = TouchableOpacityProps & {
+    title?: string;
+    className: StyleKeys;
+};
+
+export default function Button({ title, className, ...rest }: ButtonProps) {
+    let styleText;
+
+    if (className === "default") {
+        styleText = {...styles.buttonText}
+    }
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={[styles[className], styles.button]} {...rest}>
+                <Text style={styleText}>
+                    {title}
+                </Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
