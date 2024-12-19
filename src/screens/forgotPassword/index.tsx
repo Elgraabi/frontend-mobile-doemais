eedffbe49f7e9249b70f4e07cfdde296fe5d36ee
Elgraabi <silvatdb8@gmail.com>
2024-12-18 21:57:33 -0300

    feat: criação esqueceu senha screen

---
+++ src/screens/forgotPassword/index.tsx
@@ -0,0 +1,25 @@
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import React from "react";
+import { Image, Text, View } from "react-native";
+import { RoutesParams } from "../../navigation/routesParams";
+import { useNavigation } from "@react-navigation/native";
+import styles from "./styles";
+import BackButton from "../../components/buttons/backButton";
+
+type ForgotPasswordParamsList = NativeStackNavigationProp<RoutesParams, "LandingPage">;
+
+export default function ForgotPasswordScreen() {
+    const navigation = useNavigation<ForgotPasswordParamsList>();
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.backButton}>
+                <BackButton onPress={() => navigation.navigate("Login")} />
+            </View>
+            <View style={styles.logo}>
+                <Image source={require("../../../assets/Logo - Pequena.png")} />
+            </View>
+            
+        </View>
+    );
+}
