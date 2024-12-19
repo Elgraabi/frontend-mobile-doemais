9c2d658c132879e75eb051aeded411d65fab547b
Elgraabi <silvatdb8@gmail.com>
2024-12-18 21:55:54 -0300

    feat: criação do botão registrar

--- src/components/buttons/buttonRegister/index.tsx
+++ src/components/buttons/buttonRegister/index.tsx
@@ -1,10 +1,12 @@
 import { Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
 import styles from "./styles";
 
-export default function ButtonRegister() {
+interface ButtonRegisterProps extends TouchableOpacityProps {}
+
+export default function ButtonRegister({ ...rest }: ButtonRegisterProps) {
 
     return (
-        <TouchableOpacity style={styles.container}>
+        <TouchableOpacity style={styles.container} {...rest}>
             <Text style={styles.text}>
                 Cadastrar-se
             </Text>
