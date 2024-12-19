29ac2de65940872ef8e7f6005ac6278f27c36015
Elgraabi <silvatdb8@gmail.com>
2024-12-18 21:55:10 -0300

    styles: estilizando botão voltar

---
+++ src/components/buttons/backButton/styles.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    box: {
+        width: 50,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
+
+export default styles;
